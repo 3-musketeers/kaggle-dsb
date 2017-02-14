5c65913f247e09a5cd3136f61fd6773a146a29cd
LivingProgram <kyhan316@gmail.com>
2017-02-14 18:14:34 -0500

    add imports to final copy

---
+++ pipeline/build-simple-model/rough-draft/luna_mask_extraction.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, division
+import SimpleITK as sitk
+import numpy as np
+import csv
+from glob import glob
+import pandas as pd
+try:
+    from tqdm import tqdm # long waits are not fun
+except:
+    print('TQDM does make much nicer wait bars...')
+    tqdm = lambda x: x
+
