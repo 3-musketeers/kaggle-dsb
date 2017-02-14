0cbffa9e114a5ce47067f9a8de540def9db4449f
LivingProgram <kyhan316@gmail.com>
2017-02-14 18:31:52 -0500

    add code to create annotations dataframe

--- pipeline/build-simple-model/rough-draft/luna_mask_extraction.py
+++ pipeline/build-simple-model/rough-draft/luna_mask_extraction.py
@@ -17,3 +17,13 @@ LUNA_DATA_PATH = '../../../../data/luna16/'
 LUNA_SUBSET_PATH = LUNA_DATA_PATH + 'subset0/'
 OUTPUT_PATH = '../../../../output/build-simple-model'
 FILE_LIST = glob(LUNA_SUBSET_PATH + '*.mhd')
+
+# Helper function to get rows in data frame associated with each file
+def get_filename(file_list, case):
+    for f in file_list:
+        if case in f:
+            return(f)
+# The locations of the nodes
+df_node = pd.read_csv(LUNA_DATA_PATH + "annotations.csv")
+df_node["file"] = df_node["seriesuid"].map(lambda file_name: get_filename(file_list, file_name))
+df_node = df_node.dropna()
