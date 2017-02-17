14613bffed98927a25abd97c9674243046e657a1
LivingProgram <kyhan316@gmail.com>
2017-02-17 17:48:20 -0500

    add work with time and memory analysis

--- pipeline/build-simple-model/luna_mask_extraction/final_ram_limited.py
+++ pipeline/build-simple-model/luna_mask_extraction/final_ram_limited.py
@@ -10,7 +10,7 @@ try:
 except:
     print('TQDM does make much nicer wait bars...')
     tqdm = lambda x: x
-
+@profile
 # helper functions
 def make_mask(center,diam,z,width,height,spacing,origin):
     '''
@@ -43,50 +43,54 @@ def make_mask(center,diam,z,width,height,spacing,origin):
             if np.linalg.norm(center-np.array([p_x,p_y,z]))<=diam:
                 mask[int((p_y-origin[1])/spacing[1]),int((p_x-origin[0])/spacing[0])] = 1.0
     return(mask)
-
-# Getting list of image files
-LUNA_DATA_PATH = '../../../../data/luna16/'
-LUNA_SUBSET_PATH = LUNA_DATA_PATH + 'subset0/'
-OUTPUT_PATH = '../../../../output/build-simple-model'
-FILE_LIST = glob(LUNA_SUBSET_PATH + '*.mhd')
-
+@profile
 # Helper function to get rows in data frame associated with each file
 def get_filename(file_list, case):
     for f in file_list:
         if case in f:
             return(f)
-# The locations of the nodes
-df_node = pd.read_csv(LUNA_DATA_PATH + "annotations.csv")
-df_node["file"] = df_node["seriesuid"].map(lambda file_name: get_filename(FILE_LIST, file_name))
-df_node = df_node.dropna()
+@profile
+def runall():
+    # Getting list of image files
+    LUNA_DATA_PATH = '../../../../data/luna16/'
+    LUNA_SUBSET_PATH = LUNA_DATA_PATH + 'subset0/'
+    OUTPUT_PATH = '../../../../output/build-simple-model'
+    FILE_LIST = glob(LUNA_SUBSET_PATH + '*.mhd')
 
-# Looping over the image files
-for fcount, img_file in enumerate(tqdm(FILE_LIST)):
-    mini_df = df_node[df_node["file"]==img_file] # get all nodules associate with file
-    if mini_df.shape[0]>0: # some files may not have a nodule--skipping those
-        # load the data once
-        itk_img = sitk.ReadImage(img_file)
-        img_array = sitk.GetArrayFromImage(itk_img) # indexes are z,y,x (notice the ordering)
-        num_z, height, width = img_array.shape
-        origin = np.array(itk_img.GetOrigin())      # x,y,z Origin in world coordinates (mm)
-        spacing = np.array(itk_img.GetSpacing())    # spacing of voxels in world coordinates (mm)
-        # go through all nodes
-        for node_idx, cur_row in mini_df.iterrows():
-            node_x = cur_row["coordX"]
-            node_y = cur_row["coordY"]
-            node_z = cur_row["coordZ"]
-            diam = cur_row["diameter_mm"]
-            # just keep 3 slices
-            imgs = np.ndarray([3,height,width],dtype=np.float32) # defining final products
-            masks = np.ndarray([3,height,width],dtype=np.uint8)
-            center = np.array([node_x, node_y, node_z])   # nodule center
-            v_center = np.rint((center-origin)/spacing)  # nodule center in voxel space (still x,y,z ordering)
-            for i, i_z in enumerate(np.arange(int(v_center[2])-1,
-                             int(v_center[2])+2).clip(0, num_z-1)): # clip prevents going out of bounds in Z
-                mask = make_mask(center, diam, i_z*spacing[2]+origin[2],
-                                 width, height, spacing, origin)
-                masks[i] = mask
-                imgs[i] = img_array[i_z]
-            np.save(os.path.join(OUTPUT_PATH,"images_%04d_%04d.npy" % (fcount, node_idx)),imgs)
-            np.save(os.path.join(OUTPUT_PATH,"masks_%04d_%04d.npy" % (fcount, node_idx)),masks)
-    break
+
+    # The locations of the nodes
+    df_node = pd.read_csv(LUNA_DATA_PATH + "annotations.csv")
+    df_node["file"] = df_node["seriesuid"].map(lambda file_name: get_filename(FILE_LIST, file_name))
+    df_node = df_node.dropna()
+
+    # Looping over the image files
+    for fcount, img_file in enumerate(tqdm(FILE_LIST)):
+        mini_df = df_node[df_node["file"]==img_file] # get all nodules associate with file
+        if mini_df.shape[0]>0: # some files may not have a nodule--skipping those
+            # load the data once
+            itk_img = sitk.ReadImage(img_file)
+            img_array = sitk.GetArrayFromImage(itk_img) # indexes are z,y,x (notice the ordering)
+            num_z, height, width = img_array.shape
+            origin = np.array(itk_img.GetOrigin())      # x,y,z Origin in world coordinates (mm)
+            spacing = np.array(itk_img.GetSpacing())    # spacing of voxels in world coordinates (mm)
+            # go through all nodes
+            for node_idx, cur_row in mini_df.iterrows():
+                node_x = cur_row["coordX"]
+                node_y = cur_row["coordY"]
+                node_z = cur_row["coordZ"]
+                diam = cur_row["diameter_mm"]
+                # just keep 3 slices
+                imgs = np.ndarray([3,height,width],dtype=np.float32) # defining final products
+                masks = np.ndarray([3,height,width],dtype=np.uint8)
+                center = np.array([node_x, node_y, node_z])   # nodule center
+                v_center = np.rint((center-origin)/spacing)  # nodule center in voxel space (still x,y,z ordering)
+                for i, i_z in enumerate(np.arange(int(v_center[2])-1,int(v_center[2])+2).clip(0, num_z-1)): # clip prevents going out of bounds in Z
+                    mask = make_mask(center, diam, i_z*spacing[2]+origin[2],width, height, spacing, origin)
+                    masks[i] = mask
+                    imgs[i] = img_array[i_z]
+                np.save(os.path.join(OUTPUT_PATH,"images_%04d_%04d.npy" % (fcount, node_idx)),imgs)
+                np.save(os.path.join(OUTPUT_PATH,"masks_%04d_%04d.npy" % (fcount, node_idx)),masks)
+        if fcount == 1:
+            print("the program has finished")
+            break
+runall()
