54e7ebdb4d9adeefeb5f0a65d7eda6953a261023
LivingProgram <kyhan316@gmail.com>
2017-02-17 19:29:34 -0500

    add constants

--- pipeline/build-simple-model/luna-segment-lung-roi/final.py
+++ pipeline/build-simple-model/luna-segment-lung-roi/final.py
@@ -4,3 +4,6 @@ from skimage import measure
 from sklearn.cluster import KMeans
 from skimage.transform import resize
 from glob import glob
+
+WORKING_PATH = "../../../../output/build-simple-model/"
+FILE_LIST = glob(WORKING_PATH + "images_*.npy")
