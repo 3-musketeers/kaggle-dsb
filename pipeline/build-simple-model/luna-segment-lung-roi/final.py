90047a9dde7e7deabf13f5b4760b93a897ab3d00
LivingProgram <kyhan316@gmail.com>
2017-02-19 03:08:30 -0500

    add to final.py

--- pipeline/build-simple-model/luna-segment-lung-roi/final.py
+++ pipeline/build-simple-model/luna-segment-lung-roi/final.py
@@ -7,3 +7,15 @@ from glob import glob
 
 WORKING_PATH = "../../../../output/build-simple-model/"
 FILE_LIST = glob(WORKING_PATH + "images_*.npy")
+
+for img_file in FILE_LIST:
+    # I ran into an error when using Kmean on np.float16, so I'm using np.float64 here
+    imgs_to_process = np.load(img_file).astype(np.float64) 
+    print "on image", img_file
+    for i in range(len(imgs_to_process)):
+        img = imgs_to_process[i]
+        #Standardize the pixel values
+        mean = np.mean(img)
+        std = np.std(img)
+        img = img-mean
+        img = img/std
