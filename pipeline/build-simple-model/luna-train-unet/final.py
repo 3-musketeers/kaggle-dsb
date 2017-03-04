f46080473f0e45c46c6c1edf29d9f0e7148ce9ae
LivingProgram <kyhan316@gmail.com>
2017-03-04 14:56:59 -0500

    add to final.py

---
+++ pipeline/build-simple-model/luna-train-unet/final.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.optimizers import Adam
+from keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
