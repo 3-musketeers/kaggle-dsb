e42cb68dcaf14075abae7847b792a6c3fe30c622
LivingProgram <kyhan316@gmail.com>
2017-04-02 19:59:16 -0400

    add initial preprocessing of data

--- models/rev0/luna-train-unet/final.py
+++ models/rev0/luna-train-unet/final.py
@@ -15,6 +15,20 @@ IMG_COLS = 512
 K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 
 def train_and_predict():
+    print('-'*30)
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+    imgs_train = np.load(working_path+"trainImages.npy").astype(np.float32)
+    imgs_mask_train = np.load(working_path+"trainMasks.npy").astype(np.float32)
+
+    imgs_test = np.load(working_path+"testImages.npy").astype(np.float32)
+    imgs_mask_test_true = np.load(working_path+"testMasks.npy").astype(np.float32)
     
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean  # images should already be standardized, but just in case
+    imgs_train /= std
+
 if __name__ == '__main__':
     train_and_predict()
