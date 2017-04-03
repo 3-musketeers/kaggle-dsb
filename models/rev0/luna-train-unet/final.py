1ecf58a9357817b1d807796b3db44199df710add
LivingProgram <kyhan316@gmail.com>
2017-04-02 20:35:02 -0400

    add dice coefficient

--- models/rev0/luna-train-unet/final.py
+++ models/rev0/luna-train-unet/final.py
@@ -18,6 +18,14 @@ IMG_COLS = 512
 
 K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
 
 def get_unet():
     inputs = Input((1,img_rows, img_cols))
