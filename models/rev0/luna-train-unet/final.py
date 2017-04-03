c80a64aa44f52b838ef5e5ea316d6036806ce47b
LivingProgram <kyhan316@gmail.com>
2017-04-02 20:55:19 -0400

    add numpy dice coefficient

--- models/rev0/luna-train-unet/final.py
+++ models/rev0/luna-train-unet/final.py
@@ -27,6 +27,12 @@ def dice_coef(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
+def dice_coef_np(y_true,y_pred):
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
+
 def get_unet():
     inputs = Input((1,img_rows, img_cols))
     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
